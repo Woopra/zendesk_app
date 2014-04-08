c439d6c4f7552b55cbd21bbb0e286a45cb5382ef
Billy Vong <billyvg@gmail.com>
2014-04-07 18:40:47 -0700

    Clean up old example code

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 (function() {
 
+    var URL = 'https://www.woopra.com/widgets2/embed?type=profile';
+
     return {
 
         // Here we define events such as a user clicking on something
@@ -9,8 +11,12 @@
 
         // Below this point, you're free to define your own functions used by the app
         renderWoopra: function() {
+            var email = this.ticket().requester().email();
+
             this.switchTo('profile', {
-                email: this.ticket().requester().email(),
+                base_url: URL,
+                website: this.setting('website'),
+                email: email,
                 theme: 'zendesk',
                 version: new Date().getTime()
             });
