81c6882f0dffd5c0c2046a34acaf05cd3ef4f7de
Billy Vong <billyvg@gmail.com>
2014-04-21 17:58:22 -0700

    Change endpoint of embed URL, add some sanity checks

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function() {
 
-    var URL = 'https://www.woopra.com/widgets2/embed?type=profile';
+    var URL = 'https://www.woopra.com/widgets/embed?type=profile';
 
     return {
 
@@ -11,15 +11,23 @@
 
         // Below this point, you're free to define your own functions used by the app
         renderWoopra: function() {
-            var email = this.ticket().requester().email();
+            var requester = this.ticket().requester();
+            var email;
 
-            this.switchTo('profile', {
-                base_url: URL,
-                website: this.setting('website'),
-                email: email,
-                theme: 'zendesk',
-                version: new Date().getTime()
-            });
+            if (requester) {
+                email = requester.email();
+
+                if (email) {
+
+                    this.switchTo('profile', {
+                        base_url: URL,
+                        website: this.setting('website'),
+                        email: email,
+                        theme: 'zendesk',
+                        version: new Date().getTime()
+                    });
+                }
+            }
         }
     };
 
