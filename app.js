4ada499970cae20a6ad5f31570f7729fb5e6d742
Billy Vong <billyvg@gmail.com>
2014-12-22 19:19:08 -0800

    Update zendesk app to use iframe helper, fix resizing height issue

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 (function() {
 
-    var URL = 'https://www.woopra.com/widgets/embed?type=profile';
+    var URL = 'https://www.woopra.com/widgets/embed?type=profile&embed_ver=dev';
 
     return {
 
         // Here we define events such as a user clicking on something
         events: {
+            'iframe.clientReady': 'handleClientReady',
+            'iframe.setHeight': 'handleSetHeight',
             'app.activated': 'renderWoopra'
         },
 
+        handleClientReady: function(e) {
+        },
+
+        handleSetHeight: function(param) {
+            this.$('#woopra-profile iframe').css({
+                height: param.height
+            });
+        },
+
         // Below this point, you're free to define your own functions used by the app
-        renderWoopra: function() {
+        renderWoopra: function(e) {
             var requester;
             var ticket;
             var email;
@@ -26,13 +37,11 @@
             }
 
             if (email) {
-
                 this.switchTo('profile', {
                     base_url: URL,
+                    iframe_src: URL + "&website=" + this.setting('website') + "&id=" + email + "&ver=" + new Date().getTime() + "&theme=zendesk",
                     website: this.setting('website'),
-                    email: email,
-                    theme: 'zendesk',
-                    version: new Date().getTime()
+                    email: email
                 });
             }
         }
